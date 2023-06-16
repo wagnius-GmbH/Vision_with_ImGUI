622c1ba26106848c7aa16acf71e791efff841262
Florian Wagner <slvwagner@gmail.com>
2023-06-16 11:03:45 +0200

    removed vison help

--- template/vision.hpp
+++ template/vision.hpp
@@ -198,24 +198,7 @@ public:
 #include <iostream>
 //using namespace std;
 //using namespace cv;
-static void help(const char** argv)
-{
-	cout << "\nThis program demonstrates the use of cv::CascadeClassifier class to detect objects (Face + eyes). You can use Haar or LBP features.\n"
-		"This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
-		"It's most known use is for faces.\n"
-		"Usage:\n"
-		<< argv[0]
-		<< "   [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
-		"   [--nested-cascade[=nested_cascade_path this an optional secondary classifier such as eyes]]\n"
-		"   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
-		"   [--try-flip]\n"
-		"   [filename|camera_index]\n\n"
-		"example:\n"
-		<< argv[0]
-		<< " --cascade=\"data/haarcascades/haarcascade_frontalface_alt.xml\" --nested-cascade=\"data/haarcascades/haarcascade_eye_tree_eyeglasses.xml\" --scale=1.3\n\n"
-		"During execution:\n\tHit any key to quit.\n"
-		"\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
-}
+
 
 void detectAndDraw(cv::Mat& img, cv::CascadeClassifier& cascade,
 	cv::CascadeClassifier& nestedCascade,
