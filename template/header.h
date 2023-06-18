6cdf2367d28406e11c5a8533026e289173d6b837
Florian Wagner <slvwagner@gmail.com>
2023-06-18 23:59:24 +0200

    conversion functions

--- template/header.h
+++ template/header.h
@@ -46,3 +46,12 @@ using namespace std;
 #pragma warning (disable: 4996)     // 'This function or variable may be unsafe': strcpy, strdup, sprintf, vsnprintf, sscanf, fopen
 #pragma warning (disable: 26451)    // [Static Analyzer] Arithmetic overflow : Using operator 'xxx' on a 4 byte value and then casting the result to an 8 byte value. Cast the value to the wider type before calling operator 'xxx' to avoid overflow(io.2).
 #endif
+
+inline const char* convertStringToConstChar(const std::string& str) {
+    return str.c_str();
+}
+
+
+inline std::string convertIntToString(int number) {
+    return std::to_string(number);
+}
