99feec0e91629b48e6639a3e6fe1734899bbe6f4
Florian Wagner <slvwagner@gmail.com>
2023-06-19 12:13:17 +0200

    inclues moved to header

--- template/header.h
+++ template/header.h
@@ -24,13 +24,11 @@ using namespace std;
 
 // opencv dependencies 
 #include <opencv2/opencv.hpp>
-/*
-#include <opencv2/calib3d.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include "opencv2/imgcodecs.hpp"
-*/
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <iostream>
 
 // loading pictures to memory 
 #define STB_IMAGE_IMPLEMENTATION
--- template/vision.hpp
+++ template/vision.hpp
@@ -190,12 +190,6 @@ public:
 	}
 };
 
-#include "opencv2/objdetect.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/videoio.hpp"
-#include <iostream>
-
 
 class facedetection {
 private:
