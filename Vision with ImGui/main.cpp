e0065a27d3ec5322c4872bc5c39dc8627cc88f03
Florian Wagner <slvwagner@gmail.com>
2023-06-21 09:47:35 +0200

    project structure changed

---
+++ Vision with ImGui/main.cpp
@@ -0,0 +1,64 @@
+﻿// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+#include "ImGui.hpp"
+
+/// <summary>
+/// ImGui Template 
+/// </summary>
+/// <returns>int</returns>
+int main(int argc, const char** argv)
+{
+	std::cout << "Project version: " 
+		<< PROJECT_MAJOR_VERSION 
+		<< "." << PROJECT_MINOR_VERSION 
+		<< "." << PROJECT_PATCH_VERSION 
+		<< std::endl << std::endl;
+
+	// Setup window
+	if (!glfwInit())
+	{
+		cout << "errot in: glfwInit";
+		return 1;
+	}
+	
+	// GL 3.3 + GLSL 150
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Create window with graphics context
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, APP_NAME, NULL, NULL);
+	if (window == NULL)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  // tie window context to glad's opengl funcs
+		throw("Unable to context to OpenGL");
+
+	int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+	// Create GUI object
+	UseImGui myimgui;
+	myimgui.Init(window, glsl_version);
+	cout << "ImGui running" << endl;
+
+	// ImGui update
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+		myimgui.NewFrame();
+		myimgui.Update();
+		myimgui.Render();
+		glfwSwapBuffers(window);
+	}
+	myimgui.Shutdown();
+	cout << "Shutdown sucessfull";
+	return 0;
+}
+
